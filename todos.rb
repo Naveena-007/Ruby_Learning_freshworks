8859b86bf14780755fd810862a86f16d079a452f
Naveenasri Nagarajan <nanagarajan@fdlmc2402.freshworks-corp.com>
2023-12-20 11:53:19 +0530

    first commit

---
+++ todos.rb
@@ -0,0 +1,16 @@
+# ask them to list of todos 
+# ask them to print it 
+
+puts "What is your next Todo?"
+next_todo = gets
+puts "\n\n"
+puts "this is your next Todo : #{next_todo} #{next_todo}"  #String Interpolation 
+
+=begin
+learnings:
+* how to write a comment inline and multi line commaent 
+* puts and gets method to and why to use 
+* \n\n which is Carriage return 
+* String Interpolation 
+* how to asign variables 
+=end
